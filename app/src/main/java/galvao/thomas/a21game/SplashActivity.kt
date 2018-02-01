c857d7659a8f959351250db123bc9a0b8aa151e8
logonrm <logonrm@fiap.com.br>
2018-01-31 23:06:42 -0200

    Initial commit

---
+++ app/src/main/java/galvao/thomas/a21game/SplashActivity.kt
@@ -0,0 +1,41 @@
+package galvao.thomas.a21game
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var ivLogo:ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        ivLogo = findViewById(R.id.ivLogo)
+
+        carregar()
+    }
+
+    fun carregar() {
+
+        val animacao = AnimationUtils.loadAnimation( this,
+                R.anim.animacao_splash)
+
+        ivLogo.clearAnimation()
+        ivLogo.startAnimation(animacao)
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity,
+                    MainActivity::class.java)
+
+            startActivity(intent)
+
+            this@SplashActivity.finish()
+        }, 6000)
+    }
+}
